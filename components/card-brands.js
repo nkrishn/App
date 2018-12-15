c1e882fc434fce083f585e2babc76cf335a89123
Raghuvaran Reddy Julakanti <raghuvaranreddyjulakanti@Raghuvarans-MBP.fios-router.home>
2018-12-15 18:02:21 -0500

    Pinpad emv-nonemev cardbrand changes

--- components/card-brands.js
+++ components/card-brands.js
@@ -1,47 +1,67 @@
 import React from 'react'
-import { ScrollView, StatusBar, Dimensions, Text } from 'react-native'
+import { ScrollView, View, StatusBar, Dimensions, Text, StyleSheet } from 'react-native'
 import ScrollableTabView from 'react-native-scrollable-tab-view'
-import BarChart from './bar-chart'
-import { cardData} from './data'
+import LineChart from './line-chart'
 
 // in Expo - swipe left to see the following styling, or create your own
 const chartConfigs = [
-  
   {
     backgroundColor: '#ffffff',
-    backgroundGradientFrom: '#e53935',
-    backgroundGradientTo: '#ef5350',
-    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    backgroundGradientFrom: '#1E2923',
+    backgroundGradientTo: '#08130D',
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
     style: {
       borderRadius: 16
     }
-  },
+  }
 ]
 
-export default class CardBrands extends React.Component {
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+  datasets: [{
+    color: 'red',
+    data: [
+      50,
+      20,
+      2,
+      86,
+      71,
+      100
+    ]
+  },{
+    color: 'green',
+    data: [
+      20,
+      10,
+      4,
+      56,
+      87,
+      90
+    ]
+  },{
+    color: 'blue',
+    data: [
+      30,
+      90,
+      67,
+      54,
+      10,
+      2
+    ]
+  }]
+}
 
+export default class CardBrands extends React.Component {
   renderTabBar() {
-    return <StatusBar />
+    return <StatusBar hidden/>
   }
 
   render() {
-    const { width } = Dimensions.get('window')
-    const height = 220
+    const { width } = Dimensions.get('window');
+    const { height } = Dimensions.get('window') ;
     return (
-      <ScrollableTabView renderTabBar={this.renderTabBar}>
+      <View renderTabBar={this.renderTabBar}>
         {chartConfigs.map(chartConfig => {
-          const labelStyle = {
-            color: 'black',
-            marginVertical: 10,
-            textAlign: 'center',
-            fontSize: 16
-          }
-          const rankStyle = {
-            color: 'black',
-            marginVertical: 10,
-            textAlign: 'center',
-            fontSize: 20
-          }
           const graphStyle = {
             marginVertical: 8,
             ...chartConfig.style
@@ -53,19 +73,43 @@ export default class CardBrands extends React.Component {
                 backgroundColor: chartConfig.backgroundColor
               }}
             >
-              <Text style={labelStyle}> VISA vs AMEX vs DISCOVER vs MASTERCARD</Text>
-              <BarChart
-                width={width}
-                height={height}
-                data={cardData}
-                chartConfig={chartConfig}
-                style={graphStyle}
-                paddingLeft="15"
-              />
+              <Text style={{fontWeight: 'bold', color: 'red', textAlign: 'center', marginTop: 15, fontSize: 18 }}>
+                {'VISA'}{' '}
+                <Text style={{fontWeight: 'bold', color:'black'}}>
+                  {'vs'}{' '}
+                  <Text style={{fontWeight: 'bold', color: 'blue'}}>
+                    {'AMEX'}{' '}
+                    <Text style={{fontWeight: 'bold', color: 'black'}}>
+                      {'vs'}{' '}
+                      <Text style={{fontWeight: 'bold', color: 'green'}}>
+                        {'MASTERCARD'}{' '}
+                      </Text>
+                    </Text>
+                  </Text>
+                </Text>
+              </Text>
+              <View style={{ marginTop: 15}}>
+                <LineChart
+                  data={data}
+                  width={width}
+                  height={height * 75 / 100}
+                  chartConfig={chartConfig}
+                  style={graphStyle}
+                />
+              </View>
             </ScrollView>
           )
         })}
-      </ScrollableTabView>
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  labelStyle: {
+    marginVertical: 20,
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+})
